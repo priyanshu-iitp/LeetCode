cf328fc95bf4f4d6731084ee12a71218f264aef5
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-01 15:51:56 +0530

    Time: 0 ms (100%), Space: 22.1 MB (59.8%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,58 +1,29 @@
 class Solution {
 public:
+    bool ispal(string&s,int l,int r)
+    {
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            return false;
+
+            l++,r--;
+        }
+        return true;
+    }
     bool validPalindrome(string s) {
 
-        //at most 1
-        //1:-delete left char;
-        
-        int cnt=0;
         int l=0,r=s.size()-1;
-        bool flag=true;
         while(l<r)
         {
             if(s[l]!=s[r])
             {
-                cnt++,l++;
+                return ispal(s,l+1,r)||ispal(s,l,r-1);
             }
-            else 
             l++,r--;
-
-            if(cnt==2)
-            {
-                flag=false;
-                break;
-            }
         }
-        if(flag)return true;
 
-        //delete right char
-
-        cnt=0;
-        l=0,r=s.size()-1;
-        flag=true;
-        while(l<r)
-        {
-            if(s[l]!=s[r])
-            {
-                cnt++,r--;
-            }
-            else 
-            l++,r--;
-
-            if(cnt==2)
-            {
-                flag=false;
-                break;
-            }
-        }
-        if(flag)return true;
-
-        return false;
-
-
-        
-
-
+        return true;
         
     }
 };
