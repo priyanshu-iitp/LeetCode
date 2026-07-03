95e7e565404b9e193748c66a8fd2e3acb67efe9d
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-03 16:59:11 +0530

    Time: 6 ms (63.18%), Space: 11.5 MB (64.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,10 +6,9 @@ public:
         for(auto it:t)
         m[it]++;
 
+        int l=0,r=0;
+        int cnt=0,start=-1,end=s.size();
 
-        int start=-1;
-        int end=s.size();
-        int l=0,r=0,cnt=0;
         while(r<s.size())
         {
             if(m[s[r]]>0)
@@ -20,20 +19,14 @@ public:
             while(cnt==t.size())
             {
                 if(end-start>r-l)
-                {
-                    start=l,end=r;
-                }
+                start=l,end=r;
 
                 m[s[l]]++;
-
                 if(m[s[l]]>0)
                 cnt--;
-                
+
                 l++;
             }
-
-
-            
 
             r++;
         }
