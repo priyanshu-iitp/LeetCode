1a66497133090c72fb53e92167dabf1f038c00ae
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-04 13:24:00 +0530

    Time: 0 ms (100%), Space: 36.4 MB (20.28%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,18 +4,18 @@ public:
 
 
         int n=nums.size();
-        vector<int>left(n,0);
         vector<int>right(n,0);
-
-        for(int i=1;i<n;i++)
-        left[i]=nums[i-1]+left[i-1];
 
         for(int i=n-2;i>=0;i--)
         right[i]=nums[i+1]+right[i+1];
 
+        int left=0;
         for(int i=0;i<n;i++)
         {
-            if(right[i]==left[i])
+            if(i>0)
+            left=left+nums[i-1];
+
+            if(right[i]==left)
             return i;
         }
 
