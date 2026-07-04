5279cee971f9615d3f8064a456d842fa1f6bbceb
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-04 15:21:18 +0530

    Time: 0 ms (100%), Space: 13.9 MB (23.24%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -4,24 +4,33 @@ public:
 
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>>ans(m,vector<int>(n,0));
+        vector<vector<int>>prefix(m+1,vector<int>(n+1));
 
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
+        }
+
+
+        vector<vector<int>>ans(m,vector<int>(n));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                int sum=0;
-                for(int r=i-k;r<=i+k;r++)
-                {
-                    for(int c=j-k;c<=j+k;c++)
-                    {
-                        if(r>=0 && r<m && c>=0 && c<n)
-                        sum+=mat[r][c];
-                    }
-                }
-                ans[i][j]=sum;
+                int r1=max(0,i-k);
+                int c1=max(0,j-k);
+                int r2=min(m-1,i+k);
+                int c2=min(n-1,j+k);
+
+                r1++,c1++,r2++,c2++;
+
+                ans[i][j]=prefix[r2][c2]-prefix[r1-1][c2]-prefix[r2][c1-1]+prefix[r1-1][c1-1];
+    
             }
         }
         return ans;
+
+        
     }
 };
