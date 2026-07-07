b700ca88b3a1f4c9814c11a657ed0c6de00039b1
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-07 18:38:02 +0530

    Time: 17 ms (70.39%), Space: 277.9 MB (68.88%) - LeetHub

--- 3964-minimum-lights-to-illuminate-a-road/3964-minimum-lights-to-illuminate-a-road.cpp
+++ 3964-minimum-lights-to-illuminate-a-road/3964-minimum-lights-to-illuminate-a-road.cpp
@@ -3,73 +3,38 @@ public:
     int minLights(vector<int>& lights) {
 
         int n=lights.size();
-        vector<int>dup(n);
-        vector<vector<int>>inter;
-        for(int i=0;i<n;i++)
-        dup[i]=lights[i];
+        vector<int>dup(n+1,0);
 
         for(int i=0;i<n;i++)
-        {   
-            if(lights[i]!=0)
-            {
-                int l=max(0,i-lights[i]);
-                int j=min(n-1,i+lights[i]);
-                inter.push_back({l,j});
-            }
-        }
-
-        //merge interval
-        sort(inter.begin(),inter.end());
-        vector<vector<int>>ans;
-        int m=inter.size();
-        if(m>0)
         {
-            int start=inter[0][0];
-            int end=inter[0][1];
-            for(int i=1;i<m;i++)
-            {
-                if(end>=inter[i][0])
-                {
-                    end=max(end,inter[i][1]);
-                }
-                else
-                {
-                    ans.push_back({start,end});
-                    start=inter[i][0];
-                    end=inter[i][1];
-                }
-            }
-            ans.push_back({start,end});
+            if(lights[i]==0) continue;
 
+            int l=max(0,i-lights[i]);
+            int r=min(n-1,i+lights[i]);
+
+            dup[l]++;
+            
+            if(r+1<n)
+            dup[r+1]--;
         }
 
-        for(auto it:ans)
-        {
-            for(int i=it[0];i<=it[1];i++)
-            dup[i]=-1;
-        }
-
+        int dist=0;
+        int ans=0;
         int cnt=0;
-        int res=0;
         for(int i=0;i<n;i++)
         {
-            if(dup[i]==0)cnt++;
-            else 
+            dist+=dup[i];
+            if(dist==0)
+            cnt++;
+            else
             {
-                if(cnt>0)
-                res++;
-
+                ans+=(cnt+2)/3;
                 cnt=0;
             }
-
-            if(cnt==3)
-            res++,cnt=0;
         }
+        ans+=(cnt+2)/3;
 
-        if(cnt>0)
-        res++;
-
-        return res;
+        return ans;
         
     }
 };
