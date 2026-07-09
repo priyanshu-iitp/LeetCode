06d13cd9edc7565fe47d4a63af7cac6fb54346d3
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-10 00:46:43 +0530

    Time: 0 ms (100%), Space: 32.3 MB (43.34%) - LeetHub

--- 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.cpp
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.cpp
@@ -5,12 +5,7 @@ public:
         int n=nums.size();
         if(n<=2)
         return nums;
-        //left max;
-        vector<int>left(n);
-        left[0]=0;
-        left[n-1]=0;
-        for(int i=1;i<=n-2;i++)
-        left[i]=max(left[i-1],nums[i-1]);
+
 
         //right max
         vector<int>right(n);
@@ -20,10 +15,13 @@ public:
         right[i]=max(right[i+1],nums[i+1]);
 
         vector<int>ans;
+        int left=0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]>left[i] || nums[i]>right[i])
+            if(nums[i]>left || nums[i]>right[i])
             ans.push_back(nums[i]);
+
+            left=max(left,nums[i]);
         }
         return ans;
 
