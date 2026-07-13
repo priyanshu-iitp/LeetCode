c72098066525ddb035fb9577df80faf3de34faef
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-13 21:59:04 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.57%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,56 +2,40 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
+        //optimal solution (binary search)
         int n1=nums1.size();
         int n2=nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+
+        int low=0,high=n1;
+        int left=(n1+n2+1)/2;
         int n=n1+n2;
 
-        int idx2=n/2;
-        int idx1=idx2-1;
-
-        int f=0,s=0;
-        int cnt=0;
-        int eleidx1=0,eleidx2=0;
-        while(f<n1 && s<n2)
+        while(low<=high)
         {
-            if(nums1[f]<=nums2[s])
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+
+            if(l1<=r2 && l2<=r1)
             {
-                if(cnt==idx2)eleidx2=nums1[f];
-                if(cnt==idx1)eleidx1=nums1[f];
-                
-                f++;
-                cnt++;
+                if(n%2==1) return max(l1,l2)/1.0;
+
+                return ( max(l1,l2)+min(r1,r2) )/2.0;
             }
-            else
-            {
-                if(cnt==idx2)eleidx2=nums2[s];
-                if(cnt==idx1)eleidx1=nums2[s];
-                s++;
-                cnt++;
-            }
+            else if(l1>r2) high=mid1-1;
+            else low=mid1+1;
+
         }
-        //if nums1 left
-        while(f<n1)
-        {
-            if(cnt==idx2)eleidx2=nums1[f];
-            if(cnt==idx1)eleidx1=nums1[f];
-                
-            f++;
-            cnt++;
-        }
-        //if nums2 left
-        while(s<n2)
-        {
-            if(cnt==idx2)eleidx2=nums2[s];
-            if(cnt==idx1)eleidx1=nums2[s];
-                
-            s++;
-            cnt++;
-        } 
-
-        if(n%2==1) return eleidx2;
-
-        return (eleidx1+eleidx2)/2.0;
-
+        return 0;
+        
     }
 };
