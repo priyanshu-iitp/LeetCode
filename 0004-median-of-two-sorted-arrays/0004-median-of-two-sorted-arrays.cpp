8735a6a7c55cdf70997511bdff0251ef16ddb0af
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-13 20:52:18 +0530

    Time: 0 ms (100%), Space: 95.1 MB (79.86%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,43 +2,56 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        int n=nums1.size();
-        int m=nums2.size();
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int n=n1+n2;
 
-        vector<int>ans;
+        int idx2=n/2;
+        int idx1=idx2-1;
+
         int f=0,s=0;
-        while(f<n && s<m)
+        int cnt=0;
+        int eleidx1=0,eleidx2=0;
+        while(f<n1 && s<n2)
         {
             if(nums1[f]<=nums2[s])
             {
-                ans.push_back(nums1[f]);
+                if(cnt==idx2)eleidx2=nums1[f];
+                if(cnt==idx1)eleidx1=nums1[f];
+                
                 f++;
+                cnt++;
             }
             else
             {
-                ans.push_back(nums2[s]);
+                if(cnt==idx2)eleidx2=nums2[s];
+                if(cnt==idx1)eleidx1=nums2[s];
                 s++;
+                cnt++;
             }
         }
         //if nums1 left
-        while(f<n)
+        while(f<n1)
         {
-            ans.push_back(nums1[f]);
+            if(cnt==idx2)eleidx2=nums1[f];
+            if(cnt==idx1)eleidx1=nums1[f];
+                
             f++;
+            cnt++;
         }
         //if nums2 left
-        while(s<m)
+        while(s<n2)
         {
-            ans.push_back(nums2[s]);
+            if(cnt==idx2)eleidx2=nums2[s];
+            if(cnt==idx1)eleidx1=nums2[s];
+                
             s++;
+            cnt++;
         } 
 
-        int a=ans.size();
+        if(n%2==1) return eleidx2;
 
-        if(a%2==0) 
-        return (ans[a/2]+ans[(a/2)-1])/2.0;
-
-        return (ans[a/2])/1.0;
+        return (eleidx1+eleidx2)/2.0;
 
     }
 };
