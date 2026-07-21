8bbc68d837bb93794f9e7f1faa5a42f928e36ea9
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-21 13:20:36 +0530

    Time: 3 ms (61.69%), Space: 18.5 MB (65.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,46 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* mergesort(ListNode*left,ListNode*right)
-    {
-        ListNode* dummy=new ListNode(1e9);
-        ListNode*temp=dummy;
-        while(left && right)
-        {
-            if(left->val<right->val)
-            {
-                temp->next=left;
-                temp=left;
-                left=left->next;
-            }
-            else
-            {
-                temp->next=right;
-                temp=right;
-                right=right->next;
-            }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
 
+        int k=lists.size();
+        if(k==0) return NULL;
+
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>> >p;
+
+        for(int i=0;i<k;i++)
+        {
+            if(lists[i]!=NULL) p.push({lists[i]->val,lists[i]});
         }
-        if(left) temp->next=left;
-        if(right) temp->next=right;
+
+        ListNode*dummy=new ListNode(1e9);
+        ListNode*temp=dummy;
+
+        while(!p.empty())
+        {
+            int data=p.top().first;
+            ListNode*newnode= p.top().second;
+            p.pop();
+
+
+            if(newnode->next) p.push({newnode->next->val,newnode->next});
+
+            temp->next=newnode;
+            temp=newnode;
+        }
 
         temp=dummy->next;
         delete dummy;
         return temp;
-    }
-  
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-
-        if(lists.size()==0) return NULL;
-        
-        ListNode*head=lists[0];
-        for(int i=1;i<lists.size();i++)
-        {
-            head=mergesort(head,lists[i]);
-        }
-        return head;
-
-
         
     }
 };
