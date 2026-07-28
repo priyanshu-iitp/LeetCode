7ca0cf0affecae0ace1b07170b1df28aafc80e0e
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-07-28 13:17:25 +0530

    Time: 1 ms (37.13%), Space: 94.9 MB (95.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,40 +2,48 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        //optimal solution (binary search)
+
         int n1=nums1.size();
         int n2=nums2.size();
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int total=n1+n2;
 
-        int low=0,high=n1;
-        int left=(n1+n2+1)/2;
-        int n=n1+n2;
+        int i=0,j=0;
+        int prev=0;
+        int curr=0;
 
-        while(low<=high)
+        for(int count=0;count<=total/2;count++)
         {
-            int mid1=low+(high-low)/2;
-            int mid2=left-mid1;
-
-            int l1=INT_MIN,l2=INT_MIN;
-            int r1=INT_MAX,r2=INT_MAX;
-
-            if(mid1<n1) r1=nums1[mid1];
-            if(mid2<n2) r2=nums2[mid2];
-
-            if(mid1-1>=0) l1=nums1[mid1-1];
-            if(mid2-1>=0) l2=nums2[mid2-1];
-
-            if(l1<=r2 && l2<=r1)
+            prev=curr;
+            if(i<n1 && j<n2)
             {
-                if(n%2==1) return max(l1,l2)/1.0;
-
-                return ( max(l1,l2)+min(r1,r2) )/2.0;
+                if(nums1[i]<nums2[j])
+                {
+                    curr=nums1[i];
+                    i++;
+                }
+                else
+                {
+                    curr=nums2[j];
+                    j++;
+                }
             }
-            else if(l1>r2) high=mid1-1;
-            else low=mid1+1;
-
+            else if(i<n1)
+            {
+                curr=nums1[i];
+                i++;
+            }
+            else
+            {
+                curr=nums2[j];
+                j++;
+            }
         }
-        return 0;
+
+        if(total%2)
+        return curr;
+
+
+        return (prev+curr)/2.0;
         
     }
 };
