90a76a2e655cc8037a469b13b99bb978b087849b
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-03 15:50:23 +0530

    Time: 2 ms (35.94%), Space: 15.8 MB (78.48%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,29 +5,31 @@ public:
         int m=nums.size();
         int n=nums[0].size();
 
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        vector<int>prev(n,0);
 
         for(int i=0;i<m;i++)
         {
+            vector<int>temp(n,0);
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[i][j]=nums[i][j];
+                    temp[j]=nums[i][j];
                     continue;
                 }
 
                 int up=1e9;
                 int left=1e9;
 
-                if(i>0) up=nums[i][j]+dp[i-1][j];
-                if(j>0) left=nums[i][j]+dp[i][j-1];
+                if(i>0) up=nums[i][j]+prev[j];
+                if(j>0) left=nums[i][j]+temp[j-1];
 
-                dp[i][j]=min(up,left);
+                temp[j]=min(up,left);
             }
+            prev=temp;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
         
     }
 };
