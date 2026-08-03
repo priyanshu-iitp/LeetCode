6b22d8baa985a4528cccff419d8640d905b1dc17
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-03 14:29:29 +0530

    Time: 0 ms (100%), Space: 11.9 MB (55.41%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-
-    int solve(vector<vector<int>>&nums,int i,int j,vector<vector<int>>&dp)
-    {
-        //base case;
-        if(i>=0 && j>=0 && nums[i][j]==1)return 0;
-        if(i==0 && j==0)return 1;
-        if(i<0 || j<0) return 0;
-
-        if(dp[i][j]!=-1)return dp[i][j];
-
-        int up=solve(nums,i-1,j,dp);
-        int left=solve(nums,i,j-1,dp);
-
-        return dp[i][j]=up+left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>&nums) {
 
         int m=nums.size();
         int n=nums[0].size();
 
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(nums,m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i][j]==1)continue;
+                if(i==0&&j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+
+                dp[i][j]=up+left;
+
+            }
+        }
+
+
+        return dp[m-1][n-1];
         
     }
 };
