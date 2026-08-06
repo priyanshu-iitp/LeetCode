4a931685aa97da008300cc0f12f2340c239a2461
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-06 17:42:57 +0530

    Time: 3 ms (85.58%), Space: 13 MB (61.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -9,12 +9,13 @@ public:
         if((sum-target)%2==1 || sum-target<0)return 0;
 
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(k+1,0));
+        vector<int>prev(k+1,0);
+        vector<int>temp(k+1,0);
 
         for(int i=0;i<=k;i++)
         {
-            if(i==0 && nums[0] == 0) dp[0][i]=2;
-            else if (i==0 || nums[0] == i) dp[0][i]=1;
+            if(i==0 && nums[0]==0)prev[i]=2;
+            else if (i==0 || nums[0]==i)prev[i]=1;
         }
 
         for(int i=1;i<n;i++)
@@ -22,14 +23,15 @@ public:
             for(int j=0;j<=k;j++)
             {
                 //notpick
-                int notpick=dp[i-1][j];
+                int notpick=prev[j];
                 int pick=0;
-                if(j>=nums[i])pick=dp[i-1][j-nums[i]];
+                if(j>=nums[i])pick=prev[j-nums[i]];
 
-                dp[i][j]=pick+notpick;
+                temp[j]=pick+notpick;
             }
+            prev=temp;
         }
-        return dp[n-1][k];
+        return prev[k];
 
         
     }
