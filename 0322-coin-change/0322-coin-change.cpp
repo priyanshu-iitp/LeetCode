4f430a09e98c1fb3071b8e3e3ffa10e6dd7fd314
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-06 16:04:07 +0530

    Time: 53 ms (20.26%), Space: 48.8 MB (45.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,int k,vector<vector<int>>&dp)
-    {
-        if(i==0)
-        {
-            if(k%nums[i]==0)
-            return k/nums[i];
-            else return INT_MAX;
-        }
-        if(dp[i][k]!=-1)return dp[i][k];
 
-       int notpick=solve(nums,i-1,k,dp);
-       int pick=INT_MAX;
-       if(k>=nums[i]) 
-       {
-            int x=solve(nums,i,k-nums[i],dp);
-            if(x!=INT_MAX)
-            pick=1+x;
-       }
-
-       return dp[i][k]=min(pick,notpick);
-    }
     int coinChange(vector<int>& nums, int k) {
         
         int n=nums.size();
@@ -49,14 +29,6 @@ public:
             }
         }
 
-        return dp[n-1][k]==1e8?-1:dp[n-1][k];
-
-
-
-
-
-
-       
-        
+        return dp[n-1][k]==1e8?-1:dp[n-1][k]; 
     }
 };
