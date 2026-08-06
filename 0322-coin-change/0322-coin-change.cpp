df2774256c065b40097a7087d7b1e75e9af9a056
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-06 16:03:48 +0530

    Time: 47 ms (34.96%), Space: 48.9 MB (37.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,13 +21,42 @@ public:
 
        return dp[i][k]=min(pick,notpick);
     }
-    int coinChange(vector<int>& nums, int amount) {
+    int coinChange(vector<int>& nums, int k) {
         
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int x=solve(nums,n-1,amount,dp);
+        vector<vector<int>>dp(n,vector<int>(k+1,1e8));
 
-        return x==INT_MAX?-1:x;
+        for(int i=0;i<=k;i++)
+        {
+            if(i%nums[0]==0)
+            dp[0][i]=i/nums[0];
+        }
+
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=k;j++)
+            {
+                int notpick=dp[i-1][j];
+                int pick=1e8;
+                if(j>=nums[i]) 
+                {
+                        int x=dp[i][j-nums[i]];
+                        pick=1+x;
+                }
+
+                dp[i][j]=min(pick,notpick);
+            }
+        }
+
+        return dp[n-1][k]==1e8?-1:dp[n-1][k];
+
+
+
+
+
+
+       
         
     }
 };
