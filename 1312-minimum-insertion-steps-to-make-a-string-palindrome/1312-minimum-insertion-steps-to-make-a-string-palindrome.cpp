ca4566cf8610ff01dc1a22cc7ba32579955c71c1
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-08 16:17:06 +0530

    Time: 36 ms (32.23%), Space: 31.6 MB (16.32%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    int solve(string &s,string &s2,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i==0 || j==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        //match
-        if(s[i-1]==s2[j-1]) return dp[i][j]=1+solve(s,s2,i-1,j-1,dp);
-        // not match
-        return dp[i][j]=max(solve(s,s2,i-1,j,dp),solve(s,s2,i,j-1,dp));
-    }
     int minInsertions(string s) {
 
         //core concept :-(minimum insertion is n-longest palindromic subsequence);
@@ -17,10 +7,21 @@ public:
         reverse(s2.begin(),s2.end());
         int n=s.size();
 
-        //memoazation
+        //tabulation 
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return n-solve(s,s2,n,n,dp); 
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {   
+                //match
+                if(s[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                // not match
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n-dp[n][n]; 
         
     }
 };
