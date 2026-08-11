c18c2ded0da7dc321491a6983aa7aefabcc9190e
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-11 11:23:33 +0530

    Time: 0 ms (100%), Space: 23.1 MB (29.32%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -5,15 +5,18 @@ public:
         unordered_map<int,bool>m;
         int n=nums.size();
 
-        for(auto it:nums)
-        m[it]=true;
+       
 
         int sum=nums[0];
+        bool flag=true;
+        m[nums[0]]=true;
+
         for(int i=1;i<n;i++)
-        {
-            if(nums[i]==nums[i-1]+1)
+        {   
+            m[nums[i]]=true;
+            if(nums[i]==nums[i-1]+1 && flag)
             sum+=nums[i];
-            else break;
+            else flag=false;
         }
 
         for(int i=sum;i<=51;i++)
