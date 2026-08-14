9176c52fbf8dadac1493a1722de661be0ea75d72
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-14 14:07:16 +0530

    Time: 102 ms (69.91%), Space: 13.8 MB (83.97%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i>j) return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+    // int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp)
+    // {
+    //     if(i>j) return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
 
-        int ans=INT_MIN;
-        for(int idx=i;idx<=j;idx++)
-        {
-            int cost=nums[i-1]*nums[idx]*nums[j+1]+solve(nums,i,idx-1,dp)+solve(nums,idx+1,j,dp);
-            ans=max(ans,cost);
-        }
-        return dp[i][j]=ans;
-    }
+    //     int ans=INT_MIN;
+    //     for(int idx=i;idx<=j;idx++)
+    //     {
+    //         int cost=nums[i-1]*nums[idx]*nums[j+1]+solve(nums,i,idx-1,dp)+solve(nums,idx+1,j,dp);
+    //         ans=max(ans,cost);
+    //     }
+    //     return dp[i][j]=ans;
+    // }
     int maxCoins(vector<int>& arr) {
 
         int n=arr.size();
@@ -21,8 +21,25 @@ public:
         for(int i=0;i<n;i++)
         nums[i+1]=arr[i];
         
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(nums,1,n,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j<=n;j++)
+            {
+                if(i>j) continue;
+                
+                int ans=INT_MIN;
+                for(int idx=i;idx<=j;idx++)
+                {
+                    int cost=nums[i-1]*nums[idx]*nums[j+1]+dp[i][idx-1]+dp[idx+1][j];
+                    ans=max(ans,cost);
+                }
+                dp[i][j]=ans;
+
+            }
+        }
+        return dp[1][n];
         
     }
 };
