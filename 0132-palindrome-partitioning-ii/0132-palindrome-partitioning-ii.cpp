a6ce5ec822d19abb837542b030c59d6ff7c98f00
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-14 16:11:11 +0530

    Time: 1519 ms (32.39%), Space: 9.2 MB (94.98%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,27 +9,26 @@ public:
         }
         return true;
     }
-
-    int solve(string & s,int i,int n,vector<int>&dp)
-    {
-        if(i==n)return 0;
-        if(dp[i]!=-1) return dp[i];
-
-        int mini=INT_MAX;
-        for(int j=i;j<n;j++)
-        {   
-            if(ispalindrom(s,i,j))
-            {
-                int part=1+solve(s,j+1,n,dp);
-                mini=min(mini,part);
-            }
-        }
-        return dp[i]=mini;
-    }
+    
     int minCut(string s) {
         
         int n=s.size();
-        vector<int>dp(n+1,-1);
-        return solve(s,0,n,dp)-1;
+        vector<int>dp(n+1,0);
+
+        for(int i=n-1;i>=0;i--)
+        {
+            int mini=INT_MAX;
+            for(int j=i;j<n;j++)
+            {   
+                if(ispalindrom(s,i,j))
+                {
+                    int part=1+dp[j+1];
+                    mini=min(mini,part);
+                }
+            }
+            
+            dp[i]=mini;
+        }
+        return dp[0]-1;
     }
 };
