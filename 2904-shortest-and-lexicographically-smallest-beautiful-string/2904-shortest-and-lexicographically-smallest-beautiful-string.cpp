29580a7b0d9ebef60c722da561a9aad3e754c59e
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-26 11:59:58 +0530

    Time: 0 ms (100%), Space: 9.5 MB (25.19%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int check(string s,int i,int j)
-    {
-        int ans=0;
-        for(int k=i;k<=j;k++)
-        {
-            if(s[k]=='1')ans++;
-        }
-        return ans;
-    }
+    
     string shortestBeautifulSubstring(string s, int k) {
 
         int n=s.size();
         int idx=-1;
         int len=INT_MAX;
         for(int i=0;i<n;i++)
-        {
+        {   
+            int check=0;
             for(int j=i;j<n;j++)
-            {
-                if(check(s,i,j)==k && len>j-i+1)
+            {   
+                if(s[j]=='1') check++;
+
+                if(check==k && len>j-i+1)
                 {
                     idx=i;
                     len=j-i+1;
@@ -30,10 +25,12 @@ public:
 
         vector<string>ans;
         for(int i=0;i<n;i++)
-        {
+        {   
+            int check=0;
             for(int j=i;j<n;j++)
-            {
-                if(check(s,i,j)==k && j-i+1==len)
+            {   
+                if(s[j]=='1')check++;
+                if(check==k && j-i+1==len)
                 {
                     ans.push_back(s.substr(i,len));
                 }
