b68fc5258f417a0926a4ae670296fe754c4a5d3a
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-08-27 14:51:58 +0530

    Time: 5 ms (47.13%), Space: 10.1 MB (70.06%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    string lexGreaterPermutation(string s, string target) {
+
+        vector<int>m(26,0);
+        for(auto it:s)
+        m[it-'a']++;
+
+        string ans="";
+        sort(s.begin(),s.end());
+        for(auto it:target)
+        {
+            if(m[it-'a']!=0)
+            {
+                ans.push_back(it);
+                m[it-'a']--;
+            }
+            else
+            {
+                for(int c=it-'a'+1; c<26;c++)
+                {
+                    if(m[c]>0)
+                    {
+                        ans.push_back(c+'a');
+                        m[c]--;
+
+
+                        for(int i=0;i<m.size();i++)
+                        {
+                            while(m[i])
+                            {
+                                ans.push_back('a'+i);
+                                m[i]--;
+                            }
+                        }
+
+                        return ans;
+                    }  
+                }
+                break;
+            }
+        }
+
+        //backtrack
+        for(int i=ans.size()-1;i>=0;i--)
+        {
+            m[ans[i]-'a']++;
+
+            for(int c=ans[i]-'a'+1; c<26;c++)
+            {
+                if(m[c]>0)
+                {
+                    string res=ans.substr(0,i);
+                    res.push_back(c+'a');
+                    m[c]--;
+
+
+                    for(int j=0;j<m.size();j++)
+                    {
+                        while(m[j])
+                        {
+                            res.push_back('a'+j);
+                            m[j]--;
+                        }
+                    }
+
+                    return res;
+                }  
+            }
+
+        }
+
+      
+
+        return "";
+   
+        
+    }
+};
