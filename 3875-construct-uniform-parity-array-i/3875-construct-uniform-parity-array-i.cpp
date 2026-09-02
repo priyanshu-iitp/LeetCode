a525d2b52d81a605d3095d4a58a94fa93c9d0f8f
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-09-02 07:59:39 +0530

    Time: 0 ms (100%), Space: 30.1 MB (94.95%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+
+       //if there in 1 odd element present it could make all element odd 
+       //and if no odd present then array is already cointain even element
+        return true;
+        
+    }
+};
