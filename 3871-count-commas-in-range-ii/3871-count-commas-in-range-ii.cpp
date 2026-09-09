87ec795ce50f3b5ef03264998f9fe7e9219e8dc5
Priyanshu Singh <priyanshurajpoot1708@gmail.com>
2026-09-09 09:23:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.8%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,41 +3,16 @@ public:
     long long countCommas(long long n) {
 
         long long ans=0;
-        long long x=0;
-        long long y=0;
-
-        if(n>=1000)
+        long long base=1000;
+        if(n<=999) return 0;
+        else
         {
-            x=999999;
-            y=999;
-            ans+=min(n,x)-y;
+            while(base<=n)
+            {
+                ans+=(n-base+1);
+                base*=1000;
+            }
         }
-        if(n>=1000000)
-        {
-            x=999999999;
-            y=999999;
-            ans+=(min(n,x)-y)*2;
-        }
-        if(n>=1000000000)
-        {
-            x=999999999999;
-            y=999999999;
-            ans+=(min(n,x)-y)*3;
-        }
-        if(n>=1000000000000)
-        {
-            x=999999999999999;
-            y=999999999999;
-            ans+=(min(n,x)-y)*4;
-        }
-        if(n>=1000000000000000)
-        {
-            x=999999999999999999;
-            y=999999999999999;
-            ans+=(min(n,x)-y)*5;
-        }
-        
-
 
         return ans;
         
